6983fae72a65fe34ad939bc87f7197c47605fe51
Nechaeva Natalya <nechaevanatascha@yandex.ru>
2025-03-23 12:05:10 +0300

    Added configurations

--- src/test/java/tests/ConfigTest.java
+++ src/test/java/tests/ConfigTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -7,9 +9,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
+@Tag("simple")
 public class ConfigTest extends TestBase {
 
     @Test
+    @Description("Открыть главную страницу и проверить лого")
     void openPageTest() {
         step("Открыть страницу", () -> {
             open("/");
