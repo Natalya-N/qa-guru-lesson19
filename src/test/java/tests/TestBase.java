81172c3f110bc0d203a21c8172ffac2700f35835
Nechaeva Natalya <nechaevanatascha@yandex.ru>
2025-03-28 20:14:38 +0300

    Added configurations

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -43,23 +43,14 @@ public class TestBase {
         }
     }
 
-    @BeforeEach
-    void beforeEach() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-        Selenide.closeWebDriver();
     }*/
+
+    static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://samokat.ru";
+        //Configuration.baseUrl = "https://samokat.ru";
+        Configuration.baseUrl = config.getBaseUrl();
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "128");
